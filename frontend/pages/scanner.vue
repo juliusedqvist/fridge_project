22f89f0fb4eca7b6e54c91823686efd4402c5ecc
Julius Edqvist <jedqvist09@gmail.com>
2025-05-24 15:30:25 +0200

    fast-build

--- frontend/pages/scanner.vue
+++ frontend/pages/scanner.vue
@@ -41,11 +41,11 @@ const weight = ref('N/A');
 async function addToDatabase() {
   console.log(productFields.value);
   if (productFields.value.exp_date === 0 || productFields.value.exp_date === "0") {
-    console.log("hello");
     const oneWeekLater = new Date();
     oneWeekLater.setDate(oneWeekLater.getDate() + 7);
     exp_date = oneWeekLater.toISOString().split('T')[0]; // format: YYYY-MM-DD
   }
+  console.log(productFields.value)
   const res = await axios.post('/api/add_product', productFields.value);
 }
 
