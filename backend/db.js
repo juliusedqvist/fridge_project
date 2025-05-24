bf23fb866cf22a5de971805d7558027e577cde81
Julius Edqvist <jedqvist09@gmail.com>
2025-05-24 11:46:28 +0200

    fast-build

--- backend/db.js
+++ backend/db.js
@@ -8,9 +8,4 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL database');
-});
-
 module.exports = db;
