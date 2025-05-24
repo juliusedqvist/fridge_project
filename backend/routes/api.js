62d2da8043513e3ccc17d7865f6c577d46701406
Julius Edqvist <jedqvist09@gmail.com>
2025-05-24 11:43:35 +0200

    fast-build

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const db = require('../index')
+const db = require('../db')
 
 const router = express.Router();
 
