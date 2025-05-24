11b9e2d49ae717c40a23e9277429f7c9ec34c6b9
Julius Edqvist <jedqvist09@gmail.com>
2025-05-24 11:08:08 +0200

    fast-build

--- frontend/pages/scanner.vue
+++ frontend/pages/scanner.vue
@@ -183,7 +183,7 @@ div[ref="scannerContainer"] canvas {
   padding: 19px 76px;
   text-decoration: none;
   text-shadow: 0px 1px 0px #2f6627;
-  width: 70%;
+  width: 60%;
 }
 
 .whole-button:hover {
