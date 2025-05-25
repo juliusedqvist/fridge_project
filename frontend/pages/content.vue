7c675f13365cce443a02cf5e8b90d88540dc728d
Julius Edqvist <jedqvist09@gmail.com>
2025-05-25 20:51:30 +0200

    fast-build

--- frontend/pages/content.vue
+++ frontend/pages/content.vue
@@ -1,21 +1,24 @@
 <template>
   <table-component :headers="headers" :data="tableData">
-    <template #column0="{ entity }">
-      {{ entity.Name }}
-    </template>
     <template #column1="{ entity }">
-      {{ entity.Weight }} g
+      {{ entity.Food_Id }}
     </template>
     <template #column2="{ entity }">
-      {{ entity.KcalPer100g }} kcal/100g
+      {{ entity.Name }}
     </template>
     <template #column3="{ entity }">
-      {{ entity.ProteinPer100g }} g
+      {{ entity.Weight }} g
     </template>
     <template #column4="{ entity }">
-      {{ new Date(entity.Exp_Date).toLocaleDateString() }}
+      {{ entity.KcalPer100g }} kcal/100g
     </template>
     <template #column5="{ entity }">
+      {{ entity.ProteinPer100g }} g
+    </template>
+    <template #column6="{ entity }">
+      {{ new Date(entity.Exp_Date).toLocaleDateString() }}
+    </template>
+    <template #column7="{ entity }">
       {{ entity.State }}
     </template>
   </table-component>
@@ -32,6 +35,7 @@ export default {
   data() {
     return {
       headers: [
+        'Food_Id',
         'Name',
         'Weight (g)',
         'Kcal per 100g',
