bd8a92c41eeabba5b386b92625df219d24ba6519
Julius Edqvist <jedqvist09@gmail.com>
2025-05-25 14:23:30 +0200

    fast-build

--- frontend/app.vue
+++ frontend/app.vue
@@ -65,8 +65,7 @@ body {
     box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
     height: 100%;
     /* no padding here */
-    padding: 5px;
-    padding-top: 10px;
+    padding-left: 50px;
   }
 
   .content-inner {
