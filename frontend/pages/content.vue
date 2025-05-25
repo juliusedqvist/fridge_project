45f4db8e6a1b567d74fc6cc7143a54dc0d7f58a5
Julius Edqvist <jedqvist09@gmail.com>
2025-05-25 20:23:12 +0200

    fast-build

--- frontend/pages/content.vue
+++ frontend/pages/content.vue
@@ -1,9 +1,52 @@
 <template>
-  test?
+  <table-component :headers="headers" :data="data">
+    <template #column0="{ entity }">
+      {{ entity.name }}
+    </template>
+    <template #column1="{ entity }">
+      {{ entity.age }}
+    </template>
+    <template #column2="{ entity }">
+      <li v-for="(hobbie, i) in entity.hobbies" :key="`${hobbie}-${i}`">
+        {{ hobbie }}
+      </li>
+    </template>
+  </table-component>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
+import TableComponent from '~/components/Table.vue'
+
+export default Vue.extend({
+  components: {
+    TableComponent
+  },
+
+  data: () => ({
+    headers: [
+      'Name',
+      'Age',
+      'Favourite Hobbies'
+    ],
+    data: [
+      {
+        name: 'James',
+        age: 21,
+        hobbies: ['tennis', 'coding']
+      },
+      {
+        name: 'Carol',
+        age: 22,
+        hobbies: ['gaming', 'reading', 'debating']
+      },
+      {
+        name: 'Sam',
+        age: 25,
+        hobbies: ['brunching', 'going to the gym', 'painting']
+      },
+    ],
+  })
+})
 
 </script>
-
-<style scoped></style>
