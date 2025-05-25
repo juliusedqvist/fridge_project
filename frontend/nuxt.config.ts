b00f21051a19ad616f42fe7a9bf95eb3af281f46
Julius Edqvist <jedqvist09@gmail.com>
2025-05-25 13:42:22 +0200

    fast-build

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -9,10 +9,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   server: {
-    https: {
-      key: fs.readFileSync(resolve(__dirname, 'pages/192.168.0.106-key.pem')),
-      cert: fs.readFileSync(resolve(__dirname, 'pages/192.168.0.106-cert.pem')),
-    },
     host: '0.0.0.0',
     port: 3000,
   },
